40d2fa5d1c4057ae001f3ce71a998bec785818ab
Vinicius <vinicius.marcante@kron.digital>
2022-12-07 02:25:56 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function cuidadoSpoiler() {
+    console.log
+        ("Não adianta espiar, não há nada aqui, quer dizer, talvez tenha...\n" +
+        "68747470733a2f2feGJveG9yaWdpbmFsc3dpcmU=2e636f6d2e6272");
+}
